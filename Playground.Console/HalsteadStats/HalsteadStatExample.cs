fcc29c265ceb270da0764a1fdf73f0416369f670
ajhudson <andyhudson1979@gmail.com>
2021-09-18 21:56:36 +0100

    example file for calculating halstead

---
+++ Playground.Console/HalsteadStats/HalsteadStatExample.cs
@@ -0,0 +1,21 @@
+﻿namespace Playground.Console.HalsteadStats
+{
+    public static class HalsteadStatExample
+    {
+        public static void AlterValue(int value)
+        {
+            if (value != 0)
+            {
+                if (value < 0)
+                    value += 1;
+                else
+                {
+                    if (value == 999) //special value
+                        value = 0;
+                    else //process all other positive numbers
+                        value -= 1;
+                }
+            }
+        }
+    }
+}
---
+++ Playground.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using 
+
+namespace Playground.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = int.Parse(args[0]);
+            int result = HalsteadStats.HalsteadStatExample.AlterValue(value);
+
+            System.Console.WriteLine(result);
+            System.Console.ReadLine();
+        }
+    }
+}
